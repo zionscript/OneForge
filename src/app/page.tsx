d5b38f413df4a43708def57a2b082a3ce8826a32
zionscript <wassabi1002@gmail.com>
2025-03-28 15:46:28 -0300

    Setup da landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,101 +2,31 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="items-center justify-items-center">
+      <header className="w-full sticky top-0 left-0 z-50">
+        <nav className="w-[90%] md:w-[80%] mx-auto flex items-center justify-around p-4 bg-[#00000025] backdrop-blur-sm rounded mt-2">
+          <p className="text-white text-[1.5rem] font-bold">Dialog Forge</p>
+          <ul className="flex gap-[20px]">
+            <li><a href="#get">Get started</a></li>
+            <li><a href="#">Login</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main className="flex flex-col w-full p-6 items-center justify-center">
+        <h1 className="text-center text-[3rem] sm:text-[3rem] md:text-[5rem] font-bold font-inter">Automatize seus atendimentos com chatbots.</h1>
+        <h2 className="text-center text-[1.5rem] font-[500] font-inter">saque no pix e outros metodos de pagamento e sem taxas abusivas.</h2>
+        <div className="flex justify-center w-[200px] bg-[#c06600] px-[10px] py-[5px] rounded-[8px] mt-[20px]">
+          <a className="font-bold" href="#">Get started</a>
         </div>
+        <section id="get" className="flex justify-center w-full p-4 bg-[#00000030] backdrop-blur-md rounded-[20px] mt-20">
+          <div className="flex flex-col items-center gap-[20px]">
+            <p className="text-white text-[1rem] md:text-[1.5rem] font-bold">Siga estes passos para iniciar seu bot e começar seus atendimentos:</p>
+            <Image src="/bot-steps.png" alt="bot-steps" width={916} height={219}></Image>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="bottom-0 w-full flex flex-wrap items-center justify-center">
+        <p>Dialog Forge&copy; 2025</p>
       </footer>
     </div>
   );
