b35a2c835cfdbaaf0559cd955bdf7df7f3a22815
zionscript <wassabi1002@gmail.com>
2025-06-05 11:40:17 -0300

    setup: new brand for business

--- shared/assets/images.ts
+++ shared/assets/images.ts
@@ -8,7 +8,7 @@ export const Bitcoin = "/images/payment-badges/Bitcoin.png";
 
 
 export const PaymentBadges = [{
-   method: "Amazon Pay",
+    method: "Amazon Pay",
     image: AmazonPay,
     width: 187,
     height: 75,
@@ -42,4 +42,112 @@ export const PaymentBadges = [{
     image: Bitcoin,
     width: 187,
     height: 75,
+}];
+
+///////////TECH BADGES/////////////
+
+export const Analitycs = "/images/tech-badges/Analytics.png";
+export const Android = "/images/tech-badges/Android.png";
+export const Aws = "/images/tech-badges/Aws.png";
+export const Docker = "/images/tech-badges/Docker.png";
+export const Bootstrap = "/images/tech-badges/Bootstrap.png";
+export const Css = "/images/tech-badges/Css.png";
+export const Django = "/images/tech-badges/Django.png";
+export const Git = "/images/tech-badges/Git.png";
+export const Html = "/images/tech-badges/Html.png";
+export const Insomnia = "/images/tech-badges/Insomnia.png";
+export const Mysql = "/images/tech-badges/Mysql.png";
+export const Nodejs = "/images/tech-badges/Nodejs.png";
+export const Postgres = "/images/tech-badges/Postgres.png";
+export const Python = "/images/tech-badges/Python.png";
+export const React = "/images/tech-badges/React.png";
+export const Typescript = "/images/tech-badges/Typescript.png";
+export const Wordpress = "/images/tech-badges/Wordpress.png";
+
+
+export const TechBadges = [{
+    method: "Analitycs",
+    image: Analitycs,
+    width: 57,
+    height: 56,
+}, {
+    method: "Android",
+    image: Android,
+    width: 57,
+    height: 56,
+}, {
+    method: "Aws",
+    image: Aws,
+    width: 57,
+    height: 56,
+}, {
+    method: "Docker",
+    image: Docker,
+    width: 57,
+    height: 56,
+}, {
+    method: "Bootstrap",
+    image: Bootstrap,
+    width: 58,
+    height: 56,
+}, {
+    method: "Css",
+    image: Css,
+    width: 56,
+    height: 56,
+}, {
+    method: "Django",
+    image: Django,
+    width: 56,
+    height: 56,
+}, {
+    method: "Git",
+    image: Git,
+    width: 57,
+    height: 56,
+}, {
+    method: "Html",
+    image: Html,
+    width: 56,
+    height: 56,
+}, {
+    method: "Insomnia",
+    image: Insomnia,
+    width: 57,
+    height: 56,
+}, {
+    method: "Mysql",
+    image: Mysql,
+    width: 57,
+    height: 56,
+}, {
+    method: "Nodejs",
+    image: Nodejs,
+    width: 56,
+    height: 56,
+}, {
+    method: "Postgres",
+    image: Postgres,
+    width: 58,
+    height: 56
+}, {
+    method: "Python",
+    image: Python,
+    width: 58,
+    height: 56
+}, {
+    method: "React",
+    image: React,
+    width: 57,
+    height: 56
+}, {
+    method: "Typescript",
+    image: Typescript,
+    width: 56,
+    height: 56
+}, {
+    method: "Wordpress",
+    image: Wordpress,
+    width: 56,
+    height: 56
 }];
